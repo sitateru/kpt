2ec3b2ac580458a51f81a864f2eaff0fb992f2f1
orihakat05 <tkakumakuma@gmail.com>
2017-10-15 14:16:49 +0900

    READMEにバッジを追加 ・READMEの最下部にcoverallsのバッジを追加 ・rails_helperに追加したダミーのコミットを修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ SimpleCov.start
 require 'coveralls'
 Coveralls.wear!
 
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
