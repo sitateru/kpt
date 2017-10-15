97e9c4ccc0fdb840ae9a8119f03766a20ceb9389
orihakat05 <tkakumakuma@gmail.com>
2017-10-15 12:58:19 +0900

    ・title必須チェックのテストを追加 ・statusのexpectをリファクタリング

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -2,17 +2,30 @@ require 'rails_helper'
 
 RSpec.describe IssuesController, type: :controller do
 
+  shared_examples 'status_is_ok' do
+    it { expect(subject.status).to eq 200 }
+  end
+
   describe "#index" do
     let!(:issue) { create :issue }
     subject { get :index }
-    it { expect(subject.status).to eq 200 }
+    it_behaves_like 'status_is_ok'
     it { expect(JSON.parse(subject.body)['payload'].map{ |d| d['id'] }).to include issue.id}
   end
 
   describe "#create" do
-    let(:issue_attrs) {  FactoryGirl.attributes_for(:issue).keep_if { |k, v| k =~ /body|title|status/ } }
-    subject { put :create, params: issue_attrs }
-    it { expect(subject.status).to eq 200 }
-    it { expect(JSON.parse(subject.body)['payload']['body']).to eq issue_attrs[:body] }
+    context '正常登録' do
+      let(:issue_attrs) {  FactoryGirl.attributes_for(:issue).keep_if { |k, v| k =~ /body|title|status/ } }
+      subject { put :create, params: issue_attrs }
+      it_behaves_like 'status_is_ok'
+      it { expect(JSON.parse(subject.body)['payload']['body']).to eq issue_attrs[:body] }
+    end
+
+    context 'タイトルの必須NG' do
+      let(:issue_attrs) {  FactoryGirl.attributes_for(:issue).keep_if { |k, v| k =~ /body|status/ } }
+      subject { put :create, params: issue_attrs }
+      it_behaves_like 'status_is_ok'
+      it { expect(JSON.parse(subject.body)['status']).to eq 'ng' }
+    end
   end
 end
