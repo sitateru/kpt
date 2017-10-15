13445afb04754739c2a894327def66e40c4cc0f4
orihakat05 <tkakumakuma@gmail.com>
2017-10-15 12:55:20 +0900

    ・issueにtitle必須のバリデートを追加 ・追加によりテストにエラーが発生したため、修正

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe IssuesController, type: :controller do
 
   describe "#create" do
     let(:issue_attrs) {  FactoryGirl.attributes_for(:issue).keep_if { |k, v| k =~ /body|title|status/ } }
-    subject { put :create, params: { issue: issue_attrs } }
+    subject { put :create, params: issue_attrs }
     it { expect(subject.status).to eq 200 }
-    it { expect(JSON.parse(subject.body)['payload']['body']).to eq issue_attrs['body'] }
+    it { expect(JSON.parse(subject.body)['payload']['body']).to eq issue_attrs[:body] }
   end
 end
