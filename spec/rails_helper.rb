0ed53bdd0cc56e4f6ff84cbb68b41a4d54a3ed79
orihakat05 <tkakumakuma@gmail.com>
2017-10-15 14:12:18 +0900

    謝ってCircleCIの設定をしてpushしたため確認のためダミーでコミット

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ SimpleCov.start
 require 'coveralls'
 Coveralls.wear!
 
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
