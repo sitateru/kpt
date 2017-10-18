7f377ecbed700d7d6a01c511eb3dcd4827295e6e
orihakat05 <tkakumakuma@gmail.com>
2017-10-18 20:50:47 +0900

    例外処理の追加 ・Exception ・ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+
+  rescue_from Exception, with: :rescue_internal_server_error
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
+
   def health
     render_ok
   end
@@ -12,4 +16,13 @@ class ApplicationController < ActionController::API
   def render_ng(code, message)
     render json: {status: 'ng', error_code: code, message: message}, :status => code
   end
+
+  def rescue_internal_server_error
+    render_ng(500, 'internal server error')
+  end
+
+  def rescue_not_found
+    render_ng(404, 'not found')
+  end
+
 end
