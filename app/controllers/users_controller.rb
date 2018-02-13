8508acacf0f25e8ae4abce1c3e8a36fb897154ee
tibaraki <hoge@example.com>
2018-02-14 01:38:40 +0900

    users controllerの実装 - routesの追加 - seedsの追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def index
+    render_ok(User.all)
+  end
+
+  def show
+    user = User.find(params[:id])
+    render_ok(user)
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render_ok(user)
+    else
+      render_ng(400, user.errors)
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render_ok(user)
+    else
+      render_ng(400, user.errors)
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render_ok(user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 Issue.create(title: 'みんなの進捗がリモートのためみえにく', body: '進捗が見えないとリモートワークだと不安', status: :problem)
 Issue.create(title: '朝会をする', body: '朝会にて機能の進捗を確認', status: :keep)
 Issue.create(title: 'ディスプレイをつかってみる', body: 'ディスプレイにて作業効率が上がるか確かめる', status: :try)
+
+User.create(name: '山田太郎', email: 'tyamada@example.com')
+User.create(name: '鈴木花子', email: 'hsuzuki@example.com')
