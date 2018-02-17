b03323aba67350dfe5e61a7b29be0b616ba4829e
tibaraki <hoge@example.com>
2018-02-17 17:22:48 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  #論理削除対応
+  # for soft delete
   acts_as_paranoid
   validates :name, presence: true, uniqueness: { scope: :deleted_at }
-  #emailの正当性はテストメール送信等でフォローすべきなのでここでのチェックは緩めにしておく(devise準拠)
+  # not strictly
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe UsersController, type: :controller do
     let!(:user) { create(:user) }
     subject { get :index }
     it_behaves_like 'status_is_ok'
-    it { expect(JSON.parse(subject.body)['payload'].map{ |d| d['id'] }).to include user.id }
+    it { expect(JSON.parse(subject.body)['payload'].map { |d| d['id'] }).to include user.id }
   end
   describe "#show" do
     let!(:user) { create(:user) }
@@ -23,23 +23,23 @@ RSpec.describe UsersController, type: :controller do
 
   describe "#create" do
     context "ok" do
-      let!(:user_attrs) { attributes_for(:user).keep_if{ |k, _v| k =~ /name|email/ } }
+      let!(:user_attrs) { attributes_for(:user).keep_if { |k, _v| k =~ /name|email/ } }
       subject { post :create, params: { user: user_attrs } }
       it_behaves_like 'status_is_ok'
       it do
         expect(JSON.parse(subject.body)['payload']['name']).to eq user_attrs[:name]
-        expect(JSON.parse(get(:index).body)['payload'].map{ |d| d['name'] }).to include user_attrs[:name]
+        expect(JSON.parse(get(:index).body)['payload'].map { |d| d['name'] }).to include user_attrs[:name]
       end
     end
     context "ng" do
-      let!(:user_attrs) { attributes_for(:user, email: "incorrect_address").keep_if{ |k, _v| k =~ /name|email/ } }
+      let!(:user_attrs) { attributes_for(:user, email: "incorrect_address").keep_if { |k, _v| k =~ /name|email/ } }
       subject { post :create, params: { user: user_attrs } }
       it_behaves_like 'status_is_bad_request'
       it do
         expect(JSON.parse(subject.body)['status']).to eq "ng"
         expect(JSON.parse(subject.body)['error_code']).to eq 400
         expect(JSON.parse(subject.body)['message']).to_not be_empty
-        expect(JSON.parse(get(:index).body)['payload'].map{ |d| d['name'] }).to_not include user_attrs[:name]
+        expect(JSON.parse(get(:index).body)['payload'].map { |d| d['name'] }).to_not include user_attrs[:name]
       end
     end
   end
@@ -47,25 +47,25 @@ RSpec.describe UsersController, type: :controller do
   describe "#update" do
     context "ok" do
       let!(:user) { create(:user) }
-      let!(:user_attrs_updated) { attributes_for(:user, name: "hanako").keep_if{ |k, _v| k =~ /name|email/ } }
+      let!(:user_attrs_updated) { attributes_for(:user, name: "hanako").keep_if { |k, _v| k =~ /name|email/ } }
       subject { put :update, params: { id: user.id, user: user_attrs_updated } }
       it_behaves_like 'status_is_ok'
       it do
         expect(JSON.parse(subject.body)['payload']['id']).to eq user.id
-        expect(JSON.parse(get(:show, params:{id:user.id}).body)['payload']['name']).to eq user_attrs_updated[:name]
+        expect(JSON.parse(get(:show, params: { id: user.id }).body)['payload']['name']).to eq user_attrs_updated[:name]
       end
     end
     context "ng" do
       let!(:user_already_exists) { create(:user) }
       let!(:user) { create(:user, name: "taro") }
-      let!(:user_attrs_updated) { attributes_for(:user, name: user_already_exists.name).keep_if{ |k, _v| k =~ /name|email/ } }
+      let!(:user_attrs_updated) { attributes_for(:user, name: user_already_exists.name).keep_if { |k, _v| k =~ /name|email/ } }
       subject { put :update, params: { id: user.id, user: user_attrs_updated } }
       it_behaves_like 'status_is_bad_request'
       it do
         expect(JSON.parse(subject.body)['status']).to eq "ng"
         expect(JSON.parse(subject.body)['error_code']).to eq 400
         expect(JSON.parse(subject.body)['message']).to_not be_empty
-        expect(JSON.parse(get(:show, params:{id:user.id}).body)['payload']['name']).to eq user.name
+        expect(JSON.parse(get(:show, params: { id: user.id }).body)['payload']['name']).to eq user.name
       end
     end
   end
@@ -76,7 +76,7 @@ RSpec.describe UsersController, type: :controller do
     it_behaves_like 'status_is_ok'
     it do
       expect(JSON.parse(subject.body)['payload']['id']).to eq user.id
-      expect(JSON.parse(get(:index).body)['payload'].map{ |d| d['id'] }).to_not include user.id
+      expect(JSON.parse(get(:index).body)['payload'].map { |d| d['id'] }).to_not include user.id
     end
   end
 end
