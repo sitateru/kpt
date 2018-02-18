76d828e39f1b3c879c71cdf039f50e6a57b27a12
tibaraki <hoge@example.com>
2018-02-18 20:18:42 +0900

    assignコントローラとモデルを実装

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,11 +1,20 @@
 class AssignmentsController < ApplicationController
   def index
-    render_ok(Assignment.find(params[:issue_id]))
+    render_ok(JSON.parse(Assignment.includes(:user, :issue).to_json(include: [:user, :issue])))
   end
 
-  private
+  def create
+    assignment = Assignment.new(issue_id: params[:issue_id], user_id: params[:user_id])
+    if assignment.save
+      render_ok(assignment)
+    else
+      render_ng(400, assignment.errors)
+    end
+  end
 
-  def assignment_params
-    params.require(:assignment).permit(:issue_id, :user_id)
+  def destroy
+    assignment = Assignment.find(params[:id])
+    assignment.destroy
+    render_ok(assignment)
   end
 end
