11c9dc0e6b1c7b6ff6aa865aea77fb8a1ea91d5e
tibaraki <hoge@example.com>
2018-02-18 20:18:42 +0900

    assignコントローラとモデルを実装

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,6 @@
 class Assignment < ApplicationRecord
   belongs_to :issue
   belongs_to :user
+
+  validates :issue_id, uniqueness: { scope: :user_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/health', to: 'application#health'
-  resources :issues do
-    resources :assignments, only: [:index, :create, :destroy]
-  end
+  resources :issues
+  resources :assignments, only: [:index, :create, :destroy]
   resources :users
 end
