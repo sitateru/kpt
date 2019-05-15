5729a1277ab2f75c72935474b970346c07298e43
KeisukeToyota <hm.pudding0715@gmail.com>
2019-05-16 04:12:29 +0900

    #28 FactorygirlをFactoryBotに移行

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -1,13 +1,14 @@
-FactoryGirl.define do
+# coding: utf-8
+FactoryBot.define do
   factory :issue do
-    title "問題のタイトル"
-    body '問題のbody'
-    status :problem
+    title { "問題のタイトル" }
+    body { '問題のbody' }
+    status { :problem }
   end
 
   factory :issue_update, class: Issue do
-    title "トライに更新しました"
-    body 'トライにbodyを更新しました'
-    status :try
+    title { "トライに更新しました" }
+    body { 'トライにbodyを更新しました' }
+    status { :try }
   end
 end
