5bd3c5eb4b8aee3393946eed5585ac9bfd718108
KeisukeToyota <hm.pudding0715@gmail.com>
2019-05-16 03:25:54 +0900

    #36 OpenしているIssueにアサインされているUserを削除できないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_destroy :do_not_destroy_open_issue
+
   # for soft delete
   acts_as_paranoid
   has_many :assignments
@@ -7,4 +9,13 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :deleted_at }
   # not strictly
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+
+  private
+
+  def do_not_destroy_open_issue
+    return unless issues.pluck(:is_closed).include?(false)
+
+    errors[:user] << 'This user is assigned to an open issue'
+    throw(:abort)
+  end
 end
