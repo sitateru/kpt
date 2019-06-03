f9ada7767b45e325126610d9d8e26a7c25d86c72
yuuAn <nora@yuuan.net>
2019-06-03 22:52:03 +0900

    Add unique index on group_users table in migration.

--- db/migrate/20190602164923_create_group_users.rb
+++ db/migrate/20190602164923_create_group_users.rb
@@ -5,6 +5,8 @@ class CreateGroupUsers < ActiveRecord::Migration[5.1]
       t.references :user, foreign_key: true, null: false
 
       t.timestamps
+
+      t.index [:group_id, :user_id], unique: true
     end
   end
 end
