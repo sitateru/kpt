c8875767973e12cd2433b6617c68438764197174
yuuAn <nora@yuuan.net>
2019-06-04 00:12:49 +0900

    Add tests for Group model and GroupUser model.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when name is nil' do
+    subject { build(:group, name: nil) }
+    it { is_expected.not_to be_valid }
+  end
+
+  context 'when name is an empty string' do
+    subject { build(:group, name: '') }
+    it { is_expected.not_to be_valid }
+  end
 end
--- spec/models/group_user_spec.rb
+++ spec/models/group_user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe GroupUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  context 'when group id and user id are valid' do
+    subject { build(:group_user, user_id: user.id, group_id: group.id) }
+    it { is_expected.to be_valid }
+  end
+
+  context 'when the pair of ids is not unique' do
+    before do
+      create(:group_user, user_id: user.id, group_id: group.id)
+    end
+
+    context 'create model instance' do
+      subject { build(:group_user, user_id: user.id, group_id: group.id) }
+      it { is_expected.to_not be_valid }
+    end
+
+    context 'create model instance and save' do
+      let(:group_user) do
+        build(:group_user, user_id: user.id, group_id: group.id).save!(validate: false)
+      end
+
+      it { expect { group_user.save!(validate: false) }.to raise_error ActiveRecord::RecordNotUnique }
+    end
+  end
+
+  context 'when there is no record with specified id' do
+    context 'does not exists in the users table' do
+      subject { build(:group_user, user_id: user.id + 1, group_id: group.id) }
+      it { is_expected.to_not be_valid }
+    end
+
+    context 'does not exists in the groups table' do
+      subject { build(:group_user, user_id: user.id, group_id: group.id + 1) }
+      it { is_expected.to_not be_valid }
+    end
+  end
 end
