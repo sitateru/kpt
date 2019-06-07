40272aa67b034e3599a92e25a91e2f7d7b7ce3f0
yuuAn <nora@yuuan.net>
2019-06-08 04:12:04 +0900

    Add show and update of group and user relations.

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,2 +1,19 @@
 class GroupUsersController < ApplicationController
+  def show
+    group = Group.find(params[:group_id])
+    render_ok(group.users)
+  end
+
+  def update
+    group = Group.find(params[:group_id])
+    if group.update(group_users_params)
+      render_ok(group.users)
+    else
+      render_ng(400, group.errors)
+    end
+  end
+
+  def group_users_params
+    params.require(:group).permit(user_ids: [])
+  end
 end
