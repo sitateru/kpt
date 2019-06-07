af7d0b5ca934eb7850b71ab6dabb12f87bdda877
yuuAn <nora@yuuan.net>
2019-06-08 00:36:09 +0900

    Add relations between groups and users in seeder.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,6 @@ Assignment.create(issue_id: 1, user_id: 1)
 Assignment.create(issue_id: 2, user_id: 1)
 Assignment.create(issue_id: 2, user_id: 2)
 
-Group.create(name: 'developers')
-Group.create(name: 'operators')
+Group.create(name: 'developers').user_ids = [1, 2]
+Group.create(name: 'operators').user_ids = [1]
 Group.create(name: 'testers')
