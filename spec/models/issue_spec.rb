14160209ffbc7f3ee8926936325875ba5780ae4b
yuuAn <nora@yuuan.net>
2019-06-09 02:54:14 +0900

    Add tests for relations between Issue and Tag model.

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Issue, type: :model do
     subject { build(:issue, title: '') }
     it { expect(subject).not_to be_valid }
   end
+
+  context 'relation issue and tag' do
+    let(:issue) { create(:issue) }
+    let(:tag) { create(:tag) }
+
+    subject { issue.tap { |issue| issue.tag_ids = [tag.id] }.tag_ids }
+    it { is_expected.to match_array([tag.id]) }
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Tag, type: :model do
     subject { build(:tag, name: '') }
     it { is_expected.not_to be_valid }
   end
+
+  context 'relation tag and issue' do
+    let(:tag) { create(:tag) }
+    let(:issue) { create(:issue) }
+
+    subject { tag.tap { |tag| tag.issue_ids = [issue.id] }.issue_ids }
+    it { is_expected.to match_array([issue.id]) }
+  end
 end
