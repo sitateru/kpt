3f0a98ea9b9ceb8bc1938a0f1f30399090561abc
yuuAn <nora@yuuan.net>
2019-06-09 02:50:36 +0900

    Add tests for Tag model and IssueTag model.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when name is nil' do
+    subject { build(:tag, name: nil) }
+    it { is_expected.not_to be_valid }
+  end
+
+  context 'when name is an empty string' do
+    subject { build(:tag, name: '') }
+    it { is_expected.not_to be_valid }
+  end
 end
