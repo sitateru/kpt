2c181aeea6ddfb32a1294702747972961c8a6a34
yuuAn <nora@yuuan.net>
2019-06-09 01:41:11 +0900

    Add relations between issues and tags.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,6 +3,9 @@ class Issue < ApplicationRecord
   has_many :assignments # rubocop:disable Rails/HasManyOrHasOneDependent
   has_many :users, through: :assignments
 
+  has_many :issue_tags, dependent: :destroy
+  has_many :tags, through: :issue_tags
+
   validates :title, presence: true
 
   enum status: { keep: 0, problem: 1, try: 2 }
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :issue_tags, dependent: :destroy
+  has_many :issues, through: :issue_tags
 end
