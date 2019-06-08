dd8f27fa296efdbc85c4d63c211038d74c5166af
yuuAn <nora@yuuan.net>
2019-06-09 01:48:45 +0900

    Fix that references in issue_tags table can not be NULL

--- db/migrate/20190608163414_create_issue_tags.rb
+++ db/migrate/20190608163414_create_issue_tags.rb
@@ -1,8 +1,8 @@
 class CreateIssueTags < ActiveRecord::Migration[5.1]
   def change
     create_table :issue_tags do |t|
-      t.references :issue, foreign_key: true
-      t.references :tag, foreign_key: true
+      t.references :issue, foreign_key: true, null: false
+      t.references :tag, foreign_key: true, null: false
 
       t.timestamps
 
